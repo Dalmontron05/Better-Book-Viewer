2d6c979e032fd41000d759bce6f3b1daaa5801ea
Dalmontron05 <vexr2dts85338@gmail.com>
2024-09-24 18:07:18 -0700

    initial commit

---
+++ src/client/java/com/dalmontron/betterbookviewer/BetterBookViewerClient.java
@@ -0,0 +1,10 @@
+package com.dalmontron.betterbookviewer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterBookViewerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/dalmontron/betterbookviewer/BetterBookViewerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.dalmontron.betterbookviewer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterBookViewerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
